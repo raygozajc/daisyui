81776041afb31b001340d706b6721d168e513ce4
Juan Carlos Raygoza L <raygozajc@yahoo.com>
2022-02-20 21:22:11 -0700

    Themes

--- examples/angular/tailwind.config.js
+++ examples/angular/tailwind.config.js
@@ -3,4 +3,15 @@ module.exports = {
     content: ["./src/**/*.{html,ts}"],
   },
   plugins: [require("daisyui")],
+
+  daisyui: {
+    styled: true,
+    themes: ["fantasy"],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: "dark",
+  },
+
 };
