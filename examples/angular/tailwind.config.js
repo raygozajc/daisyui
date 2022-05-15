6195790c051c4c7a98a152963798516f7e08f837
Juan Carlos Raygoza L <raygozajc@yahoo.com>
2022-05-15 12:03:08 -0700

    back

--- examples/angular/tailwind.config.js
+++ examples/angular/tailwind.config.js
@@ -1,17 +1,36 @@
+// module.exports = {
+//   purge: {
+//     content: ["./src/**/*.{html,ts}"],
+//   },
+//   plugins: [require("daisyui")],
+
+//   daisyui: {
+//     styled: true,
+//     themes: ["fantasy"], 
+//     base: true,
+//     utils: true,
+//     logs: true,
+//     rtl: false,
+//     darkTheme: "dark",
+//   },
+
+// };
+
 module.exports = {
-  purge: {
-    content: ["./src/**/*.{html,ts}"],
-  },
+  //...
+
+  // add daisyUI plugin
   plugins: [require("daisyui")],
 
+  // daisyUI config (optional)
   daisyui: {
     styled: true,
-    themes: ["fantasy"],
+    themes: true,
     base: true,
     utils: true,
     logs: true,
     rtl: false,
+    prefix: "",
     darkTheme: "dark",
   },
-
-};
+}
