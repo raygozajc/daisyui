a1133c1aff5e77e8722e11c738791c3b400d3ff7
Juan Carlos Raygoza L <raygozajc@yahoo.com>
2022-07-22 17:19:26 -0700

    Update

--- examples/angular/tailwind.config.js
+++ examples/angular/tailwind.config.js
@@ -1,36 +1,41 @@
+module.exports = {
+  purge: {
+    content: ["./src/**/*.{html,ts}"],
+  },
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    styled: true,
+    themes: ["corporate"], 
+  base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: "business",
+  },
+
+};
+
+
+// themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  
+
+
 // module.exports = {
-//   purge: {
-//     content: ["./src/**/*.{html,ts}"],
-//   },
+//   //...
+
+//   // add daisyUI plugin
 //   plugins: [require("daisyui")],
 
+//   // daisyUI config (optional)
 //   daisyui: {
 //     styled: true,
-//     themes: ["fantasy"], 
+//     themes: true,
 //     base: true,
 //     utils: true,
 //     logs: true,
 //     rtl: false,
+//     prefix: "",
 //     darkTheme: "dark",
 //   },
-
-// };
-
-module.exports = {
-  //...
-
-  // add daisyUI plugin
-  plugins: [require("daisyui")],
-
-  // daisyUI config (optional)
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
-}
+// }
